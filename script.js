7a16ab9158f6a1da691767d81a65bc1b29941978
PanjaChaitanya <panjachaitanya23@gmail.com>
2024-12-11 11:24:32 +0530

    error added

--- script.js
+++ script.js
@@ -13,11 +13,17 @@ let textFontFamily = (fontName) =>{
 let DownloadPic=()=>{
     let textContainer = document.querySelector('.textContainer');
     let c = textContainer.textContent.trim()
+    let e = document.getElementById("error")
     console.log(c)
-    html2canvas(textContainer).then(canvas=>{
-        const link = document.createElement('a')
-        link.href = canvas.toDataURL()
-        link.download = 'image.png'
-        link.click()
+    if( c !== "" ){
+        html2canvas(textContainer).then(canvas=>{
+            const link = document.createElement('a')
+            link.href = canvas.toDataURL()
+            link.download = 'image.png'
+            link.click()
+            e.innerHTML = ''
         })
+    }else{
+        e.innerHTML = "Error: Text Box is Empty"
+    }
 }
